438fa0c571c601d97c00ae7ee51d625f01a4b087
Francisco Alcalde <falcalde@dc.uba.ar>
2022-10-10 20:03:54 -0300

    initial commit

---
+++ src/actor.gd
@@ -0,0 +1,10 @@
+class_name Actor
+extends KinematicBody2D
+
+# var stats
+# var inventory
+# var skills
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
---
+++ src/player/player.gd
@@ -0,0 +1,12 @@
+class_name Player
+extends Actor
+
+var MOVE_SPEED = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
